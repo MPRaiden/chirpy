92538821bb2697bbb4e372e97618772e8d799763
Marin Peric Radan <marinpericradan1@gmail.com>
2024-01-26 09:08:19 +0100

    Updated project structure, added new healthz endpoint and corresponding handler

--- main.go
+++ main.go
@@ -19,16 +19,32 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		http.NotFound(w, req)
-	})
-
-	corsMux := middlewareCors(mux)
-
-	err := http.ListenAndServe("localhost:8080", corsMux)
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("OK"))
 	if err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+		log.Printf("Error writing response: %v", err)
+		}
 }
+
+func main() {
+    mux := http.NewServeMux()
+
+    fsPublic := http.FileServer(http.Dir("public/"))
+    // Serves index.html at the route '/app' and everything else in the 'public' folder under '/app/*'
+    mux.Handle("/app/", http.StripPrefix("/app/", fsPublic))
+
+    fsAssets := http.FileServer(http.Dir("assets/"))
+    // Serves assets under the route '/app/assets/*'
+    mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", fsAssets))
+
+    mux.HandleFunc("/healthz", healthzHandler)
+    corsMux := middlewareCors(mux)
+
+    err := http.ListenAndServe("localhost:8080", corsMux)
+    if err != nil {
+        log.Fatalf("Server failed to start: %v", err)
+    }
+}
+
