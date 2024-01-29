1e76ddea6781fa052fe8a7fbcb6fe5d54f7a1261
Marin Peric Radan <marinpericradan1@gmail.com>
2024-01-29 11:31:37 +0100

    Refactored code for greater clarity

--- main.go
+++ main.go
@@ -1,50 +1,49 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
 )
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func healthzHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte("OK"))
-	if err != nil {
-		log.Printf("Error writing response: %v", err)
-		}
+type apiConfig struct {
+	fileserverHits int
 }
 
 func main() {
-    mux := http.NewServeMux()
+	const filepathRoot = "."
+	const port = "8080"
 
-    fsPublic := http.FileServer(http.Dir("public/"))
-    // Serves index.html at the route '/app' and everything else in the 'public' folder under '/app/*'
-    mux.Handle("/app/", http.StripPrefix("/app/", fsPublic))
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
 
-    fsAssets := http.FileServer(http.Dir("assets/"))
-    // Serves assets under the route '/app/assets/*'
-    mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", fsAssets))
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
-    mux.HandleFunc("/healthz", healthzHandler)
-    corsMux := middlewareCors(mux)
+	corsMux := middlewareCors(mux)
 
-    err := http.ListenAndServe("localhost:8080", corsMux)
-    if err != nil {
-        log.Fatalf("Server failed to start: %v", err)
-    }
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
